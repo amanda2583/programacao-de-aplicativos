bdfa435339f788243d4def2e796ab6f2ca205616
amanda <amandagardin30@gmail.com>
2026-02-20 09:52:11 -0300

    Aula dia 24/06

---
+++ aluno.py
@@ -0,0 +1,9 @@
+nome =input("digite seu nome")
+telefone = int(input("digite seu telefone"))
+idade = int(input("digite sua idade"))
+cidade= input("sua cidade")
+ 
+print("ola",nome)
+print("seu telefone", telefone)
+print("sua idade",idade)
+print("sua cidade",cidade)
---
+++ matematica.py
@@ -0,0 +1,17 @@
+valor1 = int(input("digite um valor"))
+valor2= int( input(" digite o segundo valor"))
+
+soma= valor1 + valor2
+subtracao= valor1- valor2
+divisao= valor1 * valor2
+multiplicacao= valor1 / valor2
+
+print(" a soma é", soma)
+print("a subtracao é",subtracao)
+print("a divisao é", divisao)
+print("a multiplicacao é", multiplicacao)
+
+ 
+      
+
+    
