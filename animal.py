dc2619c9482fde0a25e1bc5ba9285c66ae8f3b95
amanda <amandagardin30@gmail.com>
2026-03-06 10:06:52 -0300

    06/03

---
+++ animal.py
@@ -0,0 +1,9 @@
+nome = "cavalo"
+idade = 17
+peso = "650.7
+sexo = "feminino"
+ 
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
---
+++ atividade04/01.py
@@ -0,0 +1,8 @@
+curso = input("voce concluir o curso de segurança? S/N") 
+
+if curso == "s"
+     instrutor =input("o instrutor está na sala ? S/N")
+   if  instrutor =="s":
+      print("acesso liberado")
+else:
+    print("acesso negado")
